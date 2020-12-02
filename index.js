9b9d6b16042bbe7f4e39ba9a21775f266b8985ca
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-12-02 15:15:22 -0500

    Imports express and morgan modules

---
+++ index.js
@@ -0,0 +1,26 @@
+//imports express and morgan modules
+const express = require('express'), 
+      morgan = require('morgan');
+const app = express();
+
+//GET requests
+app.get('/', (req, res) => {
+  res.send('May the force be with you!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.use(express.static('public'));
+
+//handles errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
