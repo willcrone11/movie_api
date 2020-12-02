6ef4b40231cefa171a5dbd3ce4ea63de8a46d5c5
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-12-02 15:17:03 -0500

    Adds JSON object containing Star Wars Movies

--- index.js
+++ index.js
@@ -6,6 +6,53 @@ const app = express();
 //logs requests to terminal using morgan
 app.use(morgan('common'));
 
+let topMovies = [
+    {
+        Title: 'Star Wars Episode IV – A New Hope',
+        Director: 'George Lucas'
+    },
+    {
+        Title: 'Star Wars Episode V – The Empire Strikes Back',
+        Director: 'Irvin Kershner'
+    },
+    {
+        Title: 'Star Wars Episode VI – Return of the Jedi',
+        Director: 'Richard Marquand'
+    },
+    {
+        Title: 'Star Wars Episode I – The Phantom Menace',
+        Director: 'Episode II – Attack of the Clones'
+    },
+    {
+        Title: 'Star Wars Episode II – Attack of the Clones',
+        Director: 'George Lucas'
+    },
+    {
+        Title: 'Star Wars Episode III – Revenge of the Sith',
+        Director: 'George Lucas'
+    },
+    {
+        Title: 'Star Wars Episode VII – The Force Awakens',
+        Director: 'J. J. Abrams'
+    },
+    {
+        Title: 'Star Wars Episode VIII – The Last Jedi',
+        Director: 'Rian Johnson'
+    },
+    {
+        Title: 'Star Wars Episode IX – The Rise of Skywalker',
+        Director: 'J. J. Abrams'
+    },
+    {
+        Title: 'Rogue One',
+        Director: 'Gareth Edwards'
+    },
+    {
+        Title: 'Solo',
+        Director: 'Ron Howard'
+    }
+];
+
 //GET requests
 app.get('/', (req, res) => {
   res.send('May the force be with you!');
