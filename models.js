648c4a912ba862b8f7bfa5229c554645a932cc4a
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-12-15 15:54:52 -0500

    Adds movies and users collection schema models

--- models.js
+++ models.js
@@ -1,1 +1,26 @@
 const mongoose = require('mongoose');
+
+//movies collection schema
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+  },
+  ImagePath: String,
+  Featured: Boolean
+});
+
+//users collection schema
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
