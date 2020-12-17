c8ddee1a878975e0b8c4cf2fc23250de578f9dd4
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-12-17 15:29:38 -0500

    Adds code for basic HTTP authentication for login requests, checks for username using mongoose

--- passport.js
+++ passport.js
@@ -2,3 +2,24 @@ const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
   passportJWT = require('passport-jwt');
+//defines basic HTTP authentication for login requests, checks for username using mongoose
+passport.use(new LocalStrategy({
+  usernameField: 'Username',
+  passwordField: 'Password'
+}, (username, password, callback) => {
+  console.log(username + '  ' + password);
+  Users.findOne({ Username: username }, (error, user) => {
+    if (error) {
+      console.log(error);
+      return callback(error);
+    }
+
+    if (!user) {
+      console.log('incorrect username');
+      return callback(null, false, {message: 'Incorrect username or password.'});
+    }
+
+    console.log('finished');
+    return callback(null, user);
+  });
+}));
