0cecf61c870c9791e865543eeb43b6d848207631
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2020-12-17 15:31:18 -0500

    Adds code to generate JWT

--- auth.js
+++ auth.js
@@ -1,5 +1,15 @@
+const jwtSecret = 'your_jwt_secret'; // Corresponds to JWTStrategy key in passport.js
+
 const jwt = require('jsonwebtoken'),
       passport = require('passport');
 
 require('./passport');
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, //the username being encoded in the JWT
+    expiresIn: '7d', //specifies that the token will expire in 7 days
+    algorithm: 'HS256' //the algorithm used to encode the values of the JWT
+  });
+}
 
