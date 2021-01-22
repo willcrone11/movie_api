b2cd94aac5b88d14ef39ca53d50670d45651ccd4
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-01-22 14:07:45 -0500

    Adds get request to get user information

--- index.js
+++ index.js
@@ -95,6 +95,18 @@ app.get('/movies/Directors/:Name', passport.authenticate('jwt', { session: false
     });
 });
 
+//gets user information
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 //POST requests:
 //Allow new users to register
 
